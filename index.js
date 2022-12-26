26aa119f6e45cf22600b0e1d2c47b760a1ac73be
Adriano Santos <adriano_angelo@live.com>
2022-12-26 12:13:11 -0300

    Creating the getFile function that read the text file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('Hello world!'));
+const getFile = async (path) => {
+    try {
+        const encoding = 'utf-8';
+        const result = await fs.promises.readFile(path, encoding);
+        extractLinks(result);
+    } catch(err) {
+        throw new Error(chalk.red(err.mesage));
+    }
+}
+
+getFile('arquivos/texto.md');
