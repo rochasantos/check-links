776cb3c91d9177667838cf5764627c2b5e5626b3
Adriano Santos <adriano_angelo@live.com>
2022-12-26 12:15:17 -0300

    Creating the extractLinks function that extract the links of text file

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+const extractLinks = (text) => {
+    const regex = /(http[\w\-\.\_\~\?\/\#\:]*)[\s\)\}]/gm;
+    const linksExtracted = [...text.matchAll(regex)].map(arrRegex => arrRegex[1])
+    return linksExtracted;
+}
+
 const getFile = async (path) => {
     try {
         const encoding = 'utf-8';
         const result = await fs.promises.readFile(path, encoding);
-        extractLinks(result);
+        console.log(extractLinks(result));
     } catch(err) {
         throw new Error(chalk.red(err.mesage));
     }
