c2ad773f3b5b3659f30760e28bf4d54906a9ed64
Adriano Santos <adriano_angelo@live.com>
2022-12-28 14:17:51 -0300

    Correcting error when the url is invalid.

--- src/http-validation.js
+++ src/http-validation.js
@@ -1,31 +1,25 @@
 import chalk from "chalk";
-
+ 
 async function getStatus(url) {
   try {
     const response = await fetch(url)
     const status = response.status
     return status
   } catch (error) {
-    return 'INVALID_URL'
+    return error.cause.code
   }
 }
 
 async function validateLinks(arrLinks) {
   try {
-    const result = await arrLinks.map(async link => ({link: link, status: await getStatus(link) }))
-    const resultResolved = await Promise.all(result)
-    console.log(resultResolved)
+    const result = await arrLinks.map(async link => ({
+      link: link, 
+      status: await getStatus(link) 
+    }))
+    return await Promise.all(result)     
   } catch (error) {
     console.log(chalk.bgRed.white.bold(error))
   }
 }
 
-validateLinks([
-  'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList',
-  'https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input',
-  'https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer',
-  'https://developer.mozilla.org/pt-BR/ddd',
-  'www.google.com.br'
-])
-
-// export default validateLinks;
+export default validateLinks;
